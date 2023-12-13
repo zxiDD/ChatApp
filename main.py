032bdcf16275ac94113df6d48e157bd827b6a5db
Zaid <kzaid2144@gmail.com>
2023-12-13 16:14:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received Message: "+ message)
+    if(message!="User Connected"):
+        send(message,broadcast=True)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
